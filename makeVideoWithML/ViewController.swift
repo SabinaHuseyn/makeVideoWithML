9701be5688cea95a026689970d0fe217eae326ca
SabinaHuseyn <sabinah3377@gmail.com>
2023-04-14 01:05:50 +0400

    Initial commit

---
+++ makeVideoWithML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  makeVideoWithML
+//
+//  Created by Sabina Huseynova on 14.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
